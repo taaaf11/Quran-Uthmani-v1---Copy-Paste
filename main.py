a05564e14764ebd96a17070491340cff0f7e094f
taaaf11 <taafuuu@gmail.com>
2023-12-15 21:56:45 +0500

    App basic frontend complete

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Quran Usmani Script v1 Copy Paste'
+    
+    page.theme = ft.Theme(color_scheme_seed='#01666f')  # pine green
+    
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
--- ui_components/qurancopypaste.py
+++ ui_components/qurancopypaste.py
@@ -1,20 +1,23 @@
-from .input import Input
 import flet as ft
 
 
 class QuranCopyPaste(ft.Column):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.textfield = ft.TextField(hint_text='Surah:Ayah', width=200)
+        self.textfield = ft.TextField(label='Index', hint_text='Surah:Ayah', width=350)
         self.sub_button = ft.IconButton(icon=ft.icons.CHECK_SHARP, on_click=self.get_data)
         self.input_controls = [self.textfield, self.sub_button]
-        self.quran_txt = ft.Text()
+        self.quran_txt = ft.Text(size=48)
         self.controls = [
             ft.Row(self.input_controls, alignment=ft.MainAxisAlignment.CENTER),
             self.quran_txt
         ]
-        self.alignment = ft.MainAxisAlignment.CENTER
-        self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        
+        # No matter what, this vertical alignment of column have no effect.
+        # Page has vertical alignment option, columns follow them.
+        
+        # self.alignment = ...
+        self.horizontal_alignment = ft.CrossAxisAlignment.CENTER 
     
     def get_data(self, e):
         self.quran_txt.value = self.textfield.value
