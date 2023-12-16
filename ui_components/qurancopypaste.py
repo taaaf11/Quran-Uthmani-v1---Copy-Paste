35d744efed4e3e2d576866ec02bdc3ee240fb46f
taaaf11 <taafuuu@gmail.com>
2023-12-16 16:53:16 +0500

    Add font save feature

--- ui_components/qurancopypaste.py
+++ ui_components/qurancopypaste.py
@@ -1,4 +1,5 @@
 import time
+import requests
 import flet as ft
 from .aya_dict import surah_aya
 
@@ -8,16 +9,18 @@ class QuranCopyPaste(ft.Column):
         super().__init__(*args, **kwargs)
         self.textfield = ft.TextField(label='Index', hint_text='Surah:Ayah', width=350)
         self.sub_button = ft.IconButton(icon=ft.icons.CHECK_SHARP, on_click=self.show_ayah)
-        self.copy_buttom = ft.IconButton(icon=ft.icons.COPY_SHARP, on_click=self.copy)
-        self.button = [self.sub_button, self.copy_buttom]
+        self.copy_button = ft.IconButton(icon=ft.icons.COPY_SHARP, on_click=self.copy)
+        self.save_font_button = ft.IconButton(icon=ft.icons.DOWNLOAD_SHARP, on_click=self.save_font_dialog)
+        self.buttons = [self.sub_button, self.copy_button, self.save_font_button]
         self.quran_txt = ft.Text(size=42, text_align=ft.TextAlign.CENTER)
         self.quran_txt_cont = ft.Container(self.quran_txt,
                                            margin=ft.Margin(left=200, top=10,
                                                             right=200, bottom=20))
+        self.curr_font = None
         self.font_info = ft.Text(size=15)
         self.controls = [
             self.textfield,
-            ft.Row(self.button, alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row(self.buttons, alignment=ft.MainAxisAlignment.CENTER),
             self.quran_txt_cont,
             self.font_info
         ]
@@ -38,6 +41,7 @@ class QuranCopyPaste(ft.Column):
     def show_ayah(self, e):
         self.quran_txt.value = ''  # resetting the old values that were
         self.font_info.value = ''  # set by previous function calls
+        self.quran_txt_cont.content = self.quran_txt  #
         ayah_data = surah_aya[self.textfield.value]  # in surah:ayah form
         ayah_text = ayah_data[0]
         font = ayah_data[1]
@@ -45,6 +49,7 @@ class QuranCopyPaste(ft.Column):
         self.prep_font(font)
         
         self.quran_txt.font_family = font
+        self.curr_font = font
         
         old_quran_txt_cont = self.quran_txt_cont.content  # "..." appear even when len(ayah_text) is lesser than 6
         if len(ayah_text) >= 6:  # the solution is to save the old state of container content(s)
@@ -55,7 +60,6 @@ class QuranCopyPaste(ft.Column):
             ], alignment=ft.MainAxisAlignment.CENTER)
             self.font_info.value = f'The font is: {font}.ttf'
             self.update()
-            self.quran_txt_cont.content = old_quran_txt_cont
             return
         
         self.quran_txt.value = ayah_text
@@ -64,6 +68,20 @@ class QuranCopyPaste(ft.Column):
     
     def copy(self, e):
         self.page.set_clipboard(self.quran_txt.value)
+    
+    def _save_font(self, e: ft.FilePickerResultEvent):
+        font_path = e.path
+        if not (font_file is None):
+            with open(font_path, 'wb') as font_file:
+                r = requests.get(f'https://github.com/quran/quran.com-images/raw/master/res/fonts/{self.curr_font}.TTF')
+                font_file.write(r.content)
+        return
+    
+    def save_font_dialog(self, e):
+        file_picker = ft.FilePicker(on_result=self._save_font)
+        self.page.overlay.append(file_picker)
+        self.page.update()
+        file_picker.save_file("Save Font", file_name=f'{self.curr_font}.ttf', allowed_extensions=['ttf'])
     
     def build(self):
         return self
